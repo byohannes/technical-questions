de61ec31c03f42932c7df18b349dc30dec0579ec
byohannes <byohannes44@gmail.com>
2020-10-26 23:17:25 +0000

    A js function  that returns the number of objects in an array that are equal when multiplying them by the length of the array

--- hacker-rank/js-q.js
+++ hacker-rank/js-q.js
@@ -69,3 +69,20 @@ function getSecondLargest(nums) {
   }
   return secondLargest;
 }
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ *
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+  let counter = 0;
+  for (let i = 0; i < objects.length; ++i) {
+    if (objects[i].x * objects.length === objects[i].y * objects.length) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
