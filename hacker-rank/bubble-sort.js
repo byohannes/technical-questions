6b168126199347155e05927fb83c21dfc8ec69c1
byohannes <byohannes44@gmail.com>
2020-11-05 18:11:34 +0000

    created a vanilla bubble sort function and tested it

---
+++ hacker-rank/bubble-sort.js
@@ -0,0 +1,16 @@
+let bubbleSort = (inputArr) => {
+  let len = inputArr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (inputArr[j] > inputArr[j + 1]) {
+        let tmp = inputArr[j];
+        inputArr[j] = inputArr[j + 1];
+        inputArr[j + 1] = tmp;
+      }
+    }
+  }
+  return inputArr;
+};
+
+const result = bubbleSort([5, 4, 6, 2, 3, 8]);
+console.log(result);
