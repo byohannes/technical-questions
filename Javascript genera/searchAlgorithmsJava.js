ce3c1002fb07ae130810953319318077ecc54066
byohannes <byohannes44@gmail.com>
2020-12-01 20:48:44 +0000

    linear and binary search algorith implementation in js

---
+++ Javascript genera/searchAlgorithmsJava.js
@@ -0,0 +1,122 @@
+// Java implementation of iterative Binary Search 
+class BinarySearch { 
+    // Returns index of x if it is present in arr[], 
+    // else return -1 
+    int binarySearch(int arr[], int x) 
+    { 
+        int l = 0, r = arr.length - 1; 
+        while (l <= r) { 
+            int m = l + (r - l) / 2; 
+  
+            // Check if x is present at mid 
+            if (arr[m] == x) 
+                return m; 
+  
+            // If x greater, ignore left half 
+            if (arr[m] < x) 
+                l = m + 1; 
+  
+            // If x is smaller, ignore right half 
+            else
+                r = m - 1; 
+        } 
+  
+        // if we reach here, then element was 
+        // not present 
+        return -1; 
+    } 
+  
+    // Driver method to test above 
+    public static void main(String args[]) 
+    { 
+        BinarySearch ob = new BinarySearch(); 
+        int arr[] = { 2, 3, 4, 10, 40 }; 
+        int n = arr.length; 
+        int x = 10; 
+        int result = ob.binarySearch(arr, x); 
+        if (result == -1) 
+            System.out.println("Element not present"); 
+        else
+            System.out.println("Element found at "
+                               + "index " + result); 
+    } 
+} 
+
+// Java implementation of recursive Binary Search 
+class BinarySearch { 
+    // Returns index of x if it is present in arr[l.. 
+    // r], else return -1 
+    int binarySearch(int arr[], int l, int r, int x) 
+    { 
+        if (r >= l) { 
+            int mid = l + (r - l) / 2; 
+  
+            // If the element is present at the 
+            // middle itself 
+            if (arr[mid] == x) 
+                return mid; 
+  
+            // If element is smaller than mid, then 
+            // it can only be present in left subarray 
+            if (arr[mid] > x) 
+                return binarySearch(arr, l, mid - 1, x); 
+  
+            // Else the element can only be present 
+            // in right subarray 
+            return binarySearch(arr, mid + 1, r, x); 
+        } 
+  
+        // We reach here when element is not present 
+        // in array 
+        return -1; 
+    } 
+  
+    // Driver method to test above 
+    public static void main(String args[]) 
+    { 
+        BinarySearch ob = new BinarySearch(); 
+        int arr[] = { 2, 3, 4, 10, 40 }; 
+        int n = arr.length; 
+        int x = 10; 
+        int result = ob.binarySearch(arr, 0, n - 1, x); 
+        if (result == -1) 
+            System.out.println("Element not present"); 
+        else
+            System.out.println("Element found at index " + result); 
+    } 
+} 
+
+
+// Java code for linearly searching x in arr[]. If x 
+// is present then return its location, otherwise 
+// return -1 
+
+class GFG 
+{ 
+	public static int search(int arr[], int x) 
+	{ 
+		int n = arr.length; 
+		for (int i = 0; i < n; i++) 
+		{ 
+			if (arr[i] == x) 
+				return i; 
+		} 
+		return -1; 
+	} 
+
+	// Driver code 
+	public static void main(String args[]) 
+	{ 
+		int arr[] = { 2, 3, 4, 10, 40 }; 
+		int x = 10; 
+
+		// Function call 
+		int result = search(arr, x); 
+		if (result == -1) 
+			System.out.print( 
+				"Element is not present in array"); 
+		else
+			System.out.print("Element is present at index "
+							+ result); 
+	} 
+}
